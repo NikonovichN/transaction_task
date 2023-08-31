9ff2162c85dccd0486c260a3f6f2f9c12bb4e2a9
NikonovichN <nikonovichnd@gmail.com>
2023-08-31 21:07:48 +0300

    fix: add transactions bloc to DI, remove dependencies from routes

--- lib/features/transactions/presentation/pages/add_transaction_screen.dart
+++ lib/features/transactions/presentation/pages/add_transaction_screen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get_it_mixin/get_it_mixin.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/transactions_bloc.dart';
 import 'package:transaction_task/core/core.dart';
 
-class AddTransactionScreen extends StatefulWidget {
-  const AddTransactionScreen({super.key});
+class AddTransactionScreen extends StatefulWidget
+    with GetItStatefulWidgetMixin {
+  AddTransactionScreen({super.key});
 
   @override
   State<AddTransactionScreen> createState() => _AddTransactionScreenState();
 }
 
-class _AddTransactionScreenState extends State<AddTransactionScreen> {
+class _AddTransactionScreenState extends State<AddTransactionScreen>
+    with GetItStateMixin {
   static const _sumHint = 'Enter sum of transaction';
   static const _commissionHint = 'Enter commission of transaction';
   static const _errorValidationMessage = 'All fields must be filled!';
@@ -30,110 +33,113 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TransactionsBloc, TransactionsState>(
-      builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text('Add transaction'),
-          ),
-          body: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            height: 360,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  children: [
-                    const Text('Transaction type:'),
-                    const SizedBox(width: 10),
-                    DropdownButton<String>(
-                      value: _transactionType,
-                      icon: const Icon(Icons.keyboard_arrow_down_rounded),
-                      elevation: 16,
-                      style: const TextStyle(color: Colors.blue),
-                      underline: Container(
-                        height: 2,
-                        color: Colors.blueAccent,
+    return BlocProvider<TransactionsBloc>(
+      create: (context) => get<TransactionsBloc>(),
+      child: BlocBuilder<TransactionsBloc, TransactionsState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('Add transaction'),
+            ),
+            body: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              height: 360,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [
+                      const Text('Transaction type:'),
+                      const SizedBox(width: 10),
+                      DropdownButton<String>(
+                        value: _transactionType,
+                        icon: const Icon(Icons.keyboard_arrow_down_rounded),
+                        elevation: 16,
+                        style: const TextStyle(color: Colors.blue),
+                        underline: Container(
+                          height: 2,
+                          color: Colors.blueAccent,
+                        ),
+                        onChanged: (String? value) => setState(
+                          () => _transactionType = value!,
+                        ),
+                        items: TransactionType.values
+                            .map<DropdownMenuItem<String>>(
+                              (TransactionType type) => DropdownMenuItem(
+                                value: type.name,
+                                child: Text(type.name),
+                              ),
+                            )
+                            .toList(),
                       ),
-                      onChanged: (String? value) => setState(
-                        () => _transactionType = value!,
-                      ),
-                      items: TransactionType.values
-                          .map<DropdownMenuItem<String>>(
-                            (TransactionType type) => DropdownMenuItem(
-                              value: type.name,
-                              child: Text(type.name),
-                            ),
-                          )
-                          .toList(),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _sumController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: _sumHint,
                     ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _sumController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: _sumHint,
                   ),
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: _commissionController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: _commissionHint,
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _commissionController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: _commissionHint,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      child: const Text('Add'),
-                      onPressed: () {
-                        if (_sumController.text.isEmpty ||
-                            _commissionController.text.isEmpty) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            errorSnackBar(_errorValidationMessage),
-                          );
-                          return;
-                        }
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        child: const Text('Add'),
+                        onPressed: () {
+                          if (_sumController.text.isEmpty ||
+                              _commissionController.text.isEmpty) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              errorSnackBar(_errorValidationMessage),
+                            );
+                            return;
+                          }
 
-                        context.read<TransactionsBloc>().add(
-                              TransactionsEvent.addTransaction(
-                                transaction: Transaction(
-                                  date: DateTime.now().toIso8601String(),
-                                  sum: double.parse(_sumController.text),
-                                  commission: double.parse(
-                                    _commissionController.text,
-                                  ),
-                                  type: TransactionType.values.firstWhere(
-                                    (type) => type.name == _transactionType,
+                          context.read<TransactionsBloc>().add(
+                                TransactionsEvent.addTransaction(
+                                  transaction: Transaction(
+                                    date: DateTime.now().toIso8601String(),
+                                    sum: double.parse(_sumController.text),
+                                    commission: double.parse(
+                                      _commissionController.text,
+                                    ),
+                                    type: TransactionType.values.firstWhere(
+                                      (type) => type.name == _transactionType,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            );
+                              );
 
-                        context.pop();
-                      },
-                    ),
-                    TextButton(
-                      onPressed: context.pop,
-                      child: const Text('Cancel'),
-                    )
-                  ],
-                )
-              ],
+                          context.pop();
+                        },
+                      ),
+                      TextButton(
+                        onPressed: context.pop,
+                        child: const Text('Cancel'),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
--- lib/features/transactions/presentation/pages/list_transactions_screen.dart
+++ lib/features/transactions/presentation/pages/list_transactions_screen.dart
@@ -1,30 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it_mixin/get_it_mixin.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../core/core.dart';
-import '../../data/data.dart';
-import '../../domain/domain.dart';
 import '../bloc/transactions_bloc.dart';
 import '../widgets/widgets.dart';
 
-class ListTransactionsScreen extends StatelessWidget {
-  const ListTransactionsScreen({super.key});
+class ListTransactionsScreen extends StatelessWidget with GetItMixin {
+  ListTransactionsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) {
-        final dataSource = TransactionsDataSourceImpl();
-        final repository = TransactionsRepositoryImpl(dataSource);
-
-        return TransactionsBloc(
-          initDataBaseUseCase: InitDataBaseUseCase(repository),
-          addTransactionUseCase: AddTransactionUseCase(repository),
-          getTransactionsUseCase: GetTransactionsUseCase(repository),
-          removeTransactionUseCase: RemoveTransactionUseCase(repository),
-        )..add(const TransactionsEvent.initDataBase());
-      },
+      create: (context) => get<TransactionsBloc>()
+        ..add(
+          const TransactionsEvent.initDataBase(),
+        ),
       child: DefaultTabController(
         length: 2,
         child: Scaffold(
@@ -54,10 +46,7 @@ class ListTransactionsScreen extends StatelessWidget {
             builder: (context) {
               return FloatingActionButton(
                 child: const Icon(Icons.add),
-                onPressed: () => context.push(
-                  '/$addTransactionScreen',
-                  extra: context.read<TransactionsBloc>(),
-                ),
+                onPressed: () => context.push('/$addTransactionScreen'),
               );
             },
           ),
